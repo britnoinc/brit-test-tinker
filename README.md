9d3eb9c7898b30da688f56a15410a2f377fa7f61
Brit Johnson <brit@noinc.com>
2017-04-27 16:03:59 -0400

    testing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # brit-test-tinker
 A repo to host my automation testing experiments
+
+Setup steps
+1. Install Node.js 
+2. Install protractor 
+	npm install -g protractor
+3. Run your first test via 
+	http://www.protractortest.org/#/tutorial
--- protractor_test/tooltip-test-spec.js
+++ protractor_test/tooltip-test-spec.js
@@ -1,3 +1,18 @@
 /**
  * Created by Brit on 4/27/2017.
  */
+// spec.js
+describe('qris demo test', function() {
+    it('should have a title', function () {
+        browser.get('https://qa.marylandexcels.org/login'); // Navigates to the page
+        expect(browser.getTitle()).toEqual('QRIS Program'); // checks the page title
+        it('should let me login', function () {
+            element(by.id('username')).sendKeys('admin@noinc.com'); // Selects the element username
+            element(by.id('password')).sendKeys('welcome1'); // Selects the element password
+            element(by.id('_submit')).click(); // Submits the field data
+        it('should have a footer', function() {
+            expect($('.butts').isPresent()).toBeTruthy();
+            });
+        });
+    });
+});
